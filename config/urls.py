a83d9d0eedb84954a8e36b58ea127a129d288e04
Nodar <nodar.panchenko@gmail.com>
2023-11-23 17:08:49 +0400

    docker upt

--- config/urls.py
+++ config/urls.py
@@ -39,6 +39,6 @@ urlpatterns = [
     path('users/', include('users.urls', namespace='users')),
     path('habits/', include('habits.urls', namespace='habits')),
 
-    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
